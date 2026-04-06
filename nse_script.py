7414ad376d125e3b0ea469356edc5162fbf7c0a4
vamsidropmail-ui <vamsidropmail@gmail.com>
2026-04-06 10:32:52 +0530

    Update nse_script.py

--- nse_script.py
+++ nse_script.py
@@ -1,22 +1,23 @@
 import requests
 import pandas as pd
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import datetime
+import os
 
 def run_nse_task():
     url = "https://www.nseindia.com/api/market-data-pre-open?key=FO"
 
     headers = {
         "User-Agent": "Mozilla/5.0",
-        "Accept-Language": "en-US,en;q=0.9",
         "Accept": "application/json",
         "Referer": "https://www.nseindia.com/"
     }
 
     session = requests.Session()
-
-    # Step 1: Get cookies
     session.get("https://www.nseindia.com", headers=headers)
 
-    # Step 2: Call API
     response = session.get(url, headers=headers)
     data_json = response.json()
 
@@ -24,7 +25,7 @@ def run_nse_task():
 
     for item in data_json["data"]:
         symbol = item["metadata"]["symbol"]
-        final_val = item["detail"]["preOpenMarket"]["finalPrice"]
+        final_val = item["metadata"]["lastPrice"]
 
         if float(final_val).is_integer():
             data.append({
@@ -35,3 +36,28 @@ def run_nse_task():
     if data:
         df = pd.DataFrame(data)
         send_email(df.to_html(index=False))
+
+
+def send_email(table_html):
+    sender_email = os.environ["EMAIL"]
+    password = os.environ["PASSWORD"]
+
+    recipients = [
+        "amitnamo610@gmail.com",
+        "vamsidropmail@gmail.com"
+    ]
+
+    message = MIMEMultipart()
+    message["Subject"] = f"NSE F&O Whole Numbers - {datetime.date.today()}"
+    message["From"] = sender_email
+    message["To"] = ", ".join(recipients)
+
+    message.attach(MIMEText(table_html, "html"))
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, recipients, message.as_string())
+
+
+if __name__ == "__main__":
+    run_nse_task()
